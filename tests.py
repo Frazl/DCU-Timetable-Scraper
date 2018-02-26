599238698f5e292b4425ceb95c73db03ee77abad
Sean Fradl <seanfradl@Gmail.com>
2018-02-26 14:39:27 +0000

    tidying up code

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import bs4dcu
 
 print(bs4dcu.next())
+print(bs4dcu.next("AF", "1"))
+print(bs4dcu.next())
 print(bs4dcu.gettoday())
-print(bs4dcu.run("Tue","11:00","AF","1"))
-
+print(bs4dcu.responsehandler(bs4dcu.run("Tue","11:00","AF","1")))

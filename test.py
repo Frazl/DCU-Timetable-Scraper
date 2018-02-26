899960668211443ea785b6fb52abd5f757cdb0f0
Sean Fradl <seanfradl@Gmail.com>
2018-02-26 13:44:08 +0000

    travis tests..

--- test.py
+++
@@ -1,4 +0,0 @@
-import bs4dcu
-
-test = bs4dcu.run("Thu", "11:00")
-print(bs4dcu.next())
---
+++ tests.py
@@ -0,0 +1,6 @@
+import bs4dcu
+
+print(bs4dcu.next())
+print(bs4dcu.gettoday())
+print(bs4dcu.run("Tue","11:00","AF","1"))
+
